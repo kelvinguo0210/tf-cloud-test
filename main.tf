b9045bfb9cc002136062869b189de50f0ed151e7
kelvinguo0210 <58800150+kelvinguo0210@users.noreply.github.com>
2021-10-10 23:34:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 provider "null" {
   # Configuration options
 }
+
+
+resource "null_resource" "null_resource_test" {
+  provisioner "local-exec" {
+    command = "echo test ${var.name} ..."
+  }
+}
